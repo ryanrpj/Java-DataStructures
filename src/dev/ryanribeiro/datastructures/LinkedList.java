1fc2b833948875b2ba081d77884a581beea4df08
Ryan <47592751+ryanrpj@users.noreply.github.com>
2022-07-09 23:23:20 -0300

    LinkedList: append method

---
+++ src/dev/ryanribeiro/datastructures/LinkedList.java
@@ -0,0 +1,76 @@
+package dev.ryanribeiro.datastructures;
+
+final class Node<T> {
+    private final T value;
+    Node<T> next;
+
+    Node(T value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
+
+public class LinkedList<T> {
+    private Node<T> head, tail;
+    private int length = 0;
+
+    public LinkedList() {}
+
+    @SafeVarargs
+    public LinkedList(T ...initialElements) {
+        for (T element : initialElements) {
+            this.append(element);
+        }
+    }
+
+    /**
+     * Adds an element to the end of the list.
+     * @param element Element to be added.
+     */
+    public void append(T element) {
+        final Node<T> newNode = new Node<>(element);
+
+        if (tail != null) {
+            tail.next = newNode;
+            tail = newNode;
+        } else {
+            head = tail = newNode;
+        }
+
+        length++;
+    }
+
+    /**
+     * Gets the current amount of elements within the list.
+     * @return Amount of elements.
+     */
+    public int size() {
+        return length;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append("[");
+
+        Node<T> currentNode = head;
+
+        while (currentNode != null) {
+            sb.append(currentNode);
+
+            if (currentNode.next != null)
+                sb.append(", ");
+
+            currentNode = currentNode.next;
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
+}
