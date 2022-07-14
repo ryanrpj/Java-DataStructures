34d7e2b49ae9f2f2b12e580f65eef006c326e76d
Ryan <47592751+ryanrpj@users.noreply.github.com>
2022-07-14 15:45:00 -0300

    test: DoublyLinkedList: removeLast method

--- src/test/dev/ryanribeiro/datastructures/DoublyLinkedListTest.java
+++ src/test/dev/ryanribeiro/datastructures/DoublyLinkedListTest.java
@@ -3,6 +3,7 @@ package dev.ryanribeiro.datastructures;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class DoublyLinkedListTest {
     @Test
@@ -29,5 +30,33 @@ public class DoublyLinkedListTest {
         list.append(4);
         assertEquals(2, list.size());
         assertEquals("[3, 4]", list.toString());
+    }
+
+    @Test
+    public void canRemoveTheLastElement() {
+        final DoublyLinkedList<Integer> list = new DoublyLinkedList<>(1, 2);
+
+        final int removedElement = list.removeLast();
+        assertEquals(2, removedElement);
+        assertEquals(1, list.size());
+        assertEquals("[1]", list.toString());
+
+        list.removeLast();
+        assertNull(list.removeLast());
+        assertEquals(0, list.size());
+        assertEquals("[]", list.toString());
+    }
+
+    @Test
+    public void canAppendAnElementAfterRemovingLast() {
+        final DoublyLinkedList<Integer> list = new DoublyLinkedList<>(1);
+
+        list.removeLast();
+        list.removeLast();
+        list.append(2);
+        list.append(3);
+
+        assertEquals(2, list.size());
+        assertEquals("[2, 3]", list.toString());
     }
 }
